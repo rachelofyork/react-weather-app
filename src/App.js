8e483ac31ef8843b67cce5d3f5ef7b1c29e6dc05
rachelofyork <rachelofyork@gmail.com>
2021-11-21 14:27:34 +0000

    Added autoFocus to form, footer, current location button, unit conversion link HTML

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
      <div className = "container">
         
         <Weather />
-      
+      <footer>This project was coded by <a href="https://zen-visvesvaraya-696ae0.netlify.app/index.html" alt="Link to Rachel's portfolio" target="_blank" rel="noreferrer">Rachel Hollingworth</a> and is <a href="https://github.com/rachelofyork/react-weather-app" alt="Link to code on GitHub" target="_blank" rel="noreferrer">open-sourced</a> </footer>
      </div>
     </div>
+    
   );
 }
+
 
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -5,8 +5,9 @@ export default function Weather(){
     return(
     <div className="container weather">
 <form className="mt-3 mb-2">
-               <span className="search"><input type = "search" placeholder=" Search for a city..." /></span>
+               <span className="search"><input type = "search" placeholder=" Search for a city..." autoFocus="on" /></span>
        <span className="submit"><input type="submit" value="Search" className="btn" /></span>
+       <button className="currentLocation">Current Location</button>
            
         
     </form>
@@ -18,7 +19,7 @@ export default function Weather(){
 
     <div className="row mb-3">
         <div className="col-4"> <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon"/> </div>
-        <div className="col-4 temperature">10°<span className="units">C|F</span></div>
+        <div className="col-4 temperature"><span>10</span><span className="units"><a href="#" alt="Convert to celsius" className="celsiusConversion">°C</a>|<a href="#" alt="Convert to fahrenheit" className="fahrenheitConversion">°F</a></span></div>
         <div className="col-4 weatherDetails pt-2"><strong>Precipitation: 20% <br /> Humidity: 69% <br />Wind: 10km/h </strong></div>
 
     </div>
