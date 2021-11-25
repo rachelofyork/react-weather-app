51bb264fb46d66f95adb4d3b5f9f47dced27a646
rachelofyork <rachelofyork@gmail.com>
2021-11-25 20:04:59 +0000

    Added WeatherIcons component to incorporate external animated weather icons

--- src/Weather.js
+++ src/Weather.js
@@ -19,7 +19,7 @@ function handleResponse(response){
     description: response.data.weather[0].description,
 wind: response.data.wind.speed,
 humidity: response.data.main.humidity,
-icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+icon: response.data.weather[0].icon
 
 });
        
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
-import DateFormatted from "./DateFormatted"  
+import DateFormatted from "./DateFormatted" 
+import WeatherIcons from "./WeatherIcons" 
 
 export default function WeatherInfo(props){
 
@@ -9,9 +10,9 @@ export default function WeatherInfo(props){
     <h6 className="mb-3"> <DateFormatted  date={props.data.date}/></h6>
 
     <h4 className="mb-3 weatherDescription">{props.data.description}</h4>
-
     <div className="row mb-3">
-        <div className="col-4"> <img src={props.data.icon} alt={props.data.description}/> </div>
+        
+        <div className="col-4"> <WeatherIcons code={props.data.icon} alt={props.data.description}/> </div>
         <div className="col-4 temperature"><span>{Math.round(props.data.temperature)}</span><span className="units"><a href="https://www.bbc.co.uk/news" alt="Convert to celsius" className="celsiusConversion">°C</a>|<a href="https://www.bbc.co.uk/news" alt="Convert to fahrenheit" className="fahrenheitConversion">°F</a></span></div>
         <div className="col-4 weatherDetails pt-2"><strong>Precipitation: tbc% <br /> Humidity: {props.data.humidity}% <br />Wind: {Math.round(props.data.wind)}km/h </strong></div>
 
