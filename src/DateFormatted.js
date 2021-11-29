05a9d07425e58fa714f9b305ed59c999408f6ada
rachelofyork <rachelofyork@gmail.com>
2021-11-29 14:29:54 +0000

    Improved responsiveness and changed colour of location button

--- src/DateFormatted.js
+++ src/DateFormatted.js
@@ -1,6 +1,8 @@
 
 
 export default function DateFormatted(props){
+
+
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     let day = days[props.date.getDay()];
     let hours = props.date.getHours();
